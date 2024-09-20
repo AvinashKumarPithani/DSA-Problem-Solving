8f7bf5ac31baef63ce325e52ed512ffd7b24a1ee
Pithani Avinash Kumar <avinashkumar.pithani@gmail.com>
2024-09-20 08:32:53 +0530

    Power of a number in O(logN)

---
+++ Number Theory/4. Fast Exponentiation/power.cpp
@@ -0,0 +1,40 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define endl '\n'
+#define FOR(i,a,b) for(int i=(a); i<(b); i++)
+#define FORk(i,a,b,k) for(int i=(a); i<b; i+=k)
+#define rFOR(i,a,b) for(int i=(a); i>=(b); i--)
+#define rFORk(i,a,b,k) for(int i=(a); i>=(b); i-=k)
+#define pb push_back
+typedef vector<int> vi;
+typedef vector<string> vs;
+typedef long long int ll;
+typedef unsigned long long int ull;
+typedef vector<ll> vll;
+typedef vector<ull> vull;
+
+int pow(int a, int b){
+    if(b == 1) return a;
+    int ans = pow(a, b/2);
+    if(b % 2 == 0) return ans*ans;
+    return ans*ans*a;
+}
+
+void solve() {
+    int a, b;
+    cin >> a >> b;
+    cout << pow(a, b);
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    int t = 1;
+    cin >> t;
+    while(t--) {
+        solve();
+        cout << '\n';
+    }
+    return 0;
+}
