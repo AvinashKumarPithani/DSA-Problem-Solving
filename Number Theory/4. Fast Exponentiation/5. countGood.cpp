20a4be36e0afe271cd3de6523abd977168086aeb
Pithani Avinash Kumar <avinashkumar.pithani@gmail.com>
2025-06-21 16:18:01 +0530

    Count Good Numbers

---
+++ Number Theory/4. Fast Exponentiation/5. countGood.cpp
@@ -0,0 +1,53 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define endl '\n'
+#define FOR(i,a,b) for(int i=(a); i<(b); i++)
+#define FORk(i,a,b,k) for(int i=(a); i<b; i+=k)
+#define rFOR(i,a,b) for(int i=(a); i>=(b); i--)
+#define rFORk(i,a,b,k) for(int i=(a); i>=(b); i-=k)
+#define pb push_back
+typedef vector<int> vi;
+typedef vector<string> vs;
+typedef long long int ll;
+typedef unsigned long long int ull;
+typedef vector<ll> vll;
+typedef vector<ull> vull;
+
+// https://leetcode.com/problems/count-good-numbers/
+
+const int MOD = 1e9+7;
+ll pow(int a, ll b){
+    if(b == 0) return 1;
+    ll ans = pow(a, b/2);
+    ans = (ans*ans) % MOD;
+    if(b % 2 == 1) ans = (ans*a) % MOD;
+    return ans;
+}
+
+int countGoodNumbers(long long n) {  // O(log n/2);
+    ll res = 1;
+    if(n&1) res = 5;
+    res = (res*pow(5, n/2)) % MOD;
+    res = (res*pow(4, n/2)) % MOD;
+
+    return res; 
+}
+
+void solve() {
+    ll n;
+    cin >> n;
+    cout << countGoodNumbers(n);
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    int t = 1;
+    cin >> t;
+    while(t--) {
+        solve();
+        cout << '\n';
+    }
+    return 0;
+}
