13a474c499c4da328f9db2dd36ef59b1651f24b0
Pithani Avinash Kumar <avinashkumar.pithani@gmail.com>
2025-06-22 16:21:41 +0530

    Count total Anagrams possible

---
+++ Number Theory/5. Combinatorics/3. cntAnagrams.cpp
@@ -0,0 +1,90 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define endl '\n'
+#define FOR(i,a,b) for(int i=(a); i<(b); i++)
+#define FORk(i,a,b,k) for(int i=(a); i<b; i+=k)
+#define rFOR(i,a,b) for(int i=(a); i>=(b); i--)
+#define rFORk(i,a,b,k) for(int i=(a); i>=(b); i-=k)
+#define pb push_back
+typedef vector<int> vi;
+typedef vector<string> vs;
+typedef long long int ll;
+typedef unsigned long long int ull;
+typedef vector<ll> vll;
+typedef vector<ull> vull;
+
+const int MOD = 1e9+7;
+const int N = 1e5;
+ll f[N+1];
+ll fi[N+1];
+
+ll pow(int a, int b){
+  if(b == 0) return 1;
+  ll ans = pow(a, b/2);
+  ans = (ans*ans) % MOD;
+  if(b % 2 == 1) ans = (ans*a) % MOD;
+  return ans;
+}
+
+
+ll mod_inv(int x) {
+  return pow(x, MOD-2);
+}
+
+void pre_compute() {
+  f[0] = 1;
+  FOR(i, 1, N+1) {
+    f[i] = (i*f[i-1]) % MOD;
+  }
+  fi[N] = mod_inv(f[N]);
+  rFOR(i, N-1, 0) {
+    fi[i] = ((i+1)*fi[i+1]) % MOD;
+  }
+}
+
+class Solution {
+public:
+    int countAnagrams(string s) {   // O(n)
+      int ans = 1;
+      int i = 0;
+      int n = s.length();
+      while(i < n) {
+        unordered_map <char, int> mp;
+        int c = 0;
+        while(i < n && s[i] != ' ') {
+          mp[s[i]]++;
+          c++;
+          i++;
+        }
+        ll w = f[c];
+        for(auto x: mp){
+          if(x.second > 1) w = (w*fi[x.second])% MOD;         
+        }
+        ans = (ans*w)% MOD;
+        i++;
+      }
+      return ans;
+    }
+};
+
+void solve(Solution sol) {
+    string s;
+    // cin >> s;    
+    getline(cin >> ws, s); // skips the newline, reads the next line properly
+    cout << sol.countAnagrams(s);
+}
+
+int main() {    // O(N + log MOD + t*n)
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    int t = 1;
+    cin >> t;
+    pre_compute();   // O(N + log MOD)
+    Solution obj = Solution();
+    while(t--) {
+        solve(obj);
+        cout << '\n';
+    }
+    return 0;
+}
