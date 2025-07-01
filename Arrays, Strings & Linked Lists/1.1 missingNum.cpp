974e273a9a85ccef4457f8565d3a6917213cddc0
Pithani Avinash Kumar <avinashkumar.pithani@gmail.com>
2025-07-02 00:46:16 +0530

    missing number, O(n*log n)

---
+++ Arrays, Strings & Linked Lists/1.1 missingNum.cpp
@@ -0,0 +1,53 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define endl '\n'
+#define FOR(i,a,b) for(int i=(a); i<(b); i++)
+#define FORk(i,a,b,k) for(int i=(a); i<b; i+=k)
+#define rFOR(i,a,b) for(int i=(a); i>=(b); i--)
+#define rFORk(i,a,b,k) for(int i=(a); i>=(b); i-=k)
+#define pb push_back
+typedef vector<int> vi;
+typedef vector<string> vs;
+typedef long long int ll;
+typedef unsigned long long int ull;
+typedef vector<ll> vll;
+typedef vector<ull> vull;
+const int MOD = 1e9+7;
+
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {    // O(n*log n)
+      int n = nums.size();
+      sort(nums.begin(), nums.end());
+      FOR(i, 0, n) {
+        if(nums[i] != i){
+          return i;
+        }
+      }
+      return n;
+    }
+};
+
+void solve(Solution sol) {
+    vi nums;
+    FOR(i, 0, nums.size()){
+      int x;
+      cin >> x;
+      nums.pb(x);
+    }
+    cout << sol.missingNumber(nums);
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    int t = 1;
+    cin >> t;
+    Solution obj = Solution();
+    while(t--) {
+        solve(obj);
+        cout << '\n';
+    }
+    return 0;
+}
