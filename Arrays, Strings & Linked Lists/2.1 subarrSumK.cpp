05450773d86ba6a6148c4b7c257b48f26d11fb9c
Pithani Avinash Kumar <avinashkumar.pithani@gmail.com>
2025-07-02 16:29:58 +0530

    subarray sum equals to k, O(n)

---
+++ Arrays, Strings & Linked Lists/2.1 subarrSumK.cpp
@@ -0,0 +1,59 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define endl '\n'
+#define FOR(i,a,b) for(int i=(a); i<(b); i++)
+#define FORk(i,a,b,k) for(int i=(a); i<b; i+=k)
+#define rFOR(i,a,b) for(int i=(a); i>=(b); i--)
+#define rFORk(i,a,b,k) for(int i=(a); i>=(b); i-=k)
+#define pb push_back
+typedef vector<int> vi;
+typedef vector<string> vs;
+typedef long long int ll;
+typedef unsigned long long int ull;
+typedef vector<ll> vll;
+typedef vector<ull> vull;
+typedef vector<bool> vb;
+const int MOD = 1e9+7;
+
+class Solution {
+public:
+    int subarraySum(vector<int>& nums, int k) {     // TC: O(n), SC: O(n)
+      int n = nums.size();
+      int ans = 0;
+      ll sum = 0;
+      unordered_map<int, int> m;
+      m[0]++;
+      FOR(i, 0, n) {
+        sum += nums[i];
+        ans += m[sum-k];        
+        m[sum]++;
+      }
+      return ans;
+    }
+};
+
+void solve(Solution sol) {
+    vi nums;
+    FOR(i, 0, nums.size()){
+      int x;
+      cin >> x;
+      nums.pb(x);
+    }
+    int k;
+    cin >> k;
+    cout << sol.subarraySum(nums, k);
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    int t = 1;
+    cin >> t;
+    Solution obj = Solution();
+    while(t--) {
+        solve(obj);
+        cout << '\n';
+    }
+    return 0;
+}
