077b7788267bdc795478aea9615bb848b5be4e6e
Pithani Avinash Kumar <avinashkumar.pithani@gmail.com>
2025-07-03 20:46:11 +0530

    sort colors 0,1,2;  O(n)

---
+++ Arrays, Strings & Linked Lists/2. Dutch National Flag Algorithm/1. sortColors.cpp
@@ -0,0 +1,69 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define endl '\n'
+#define FOR(i,a,b) for(int i=(a); i<(b); i++)
+#define FORk(i,a,b,k) for(int i=(a); i<b; i+=k)
+#define rFOR(i,a,b) for(int i=(a); i>=(b); i--)
+#define rFORk(i,a,b,k) for(int i=(a); i>=(b); i-=k)
+#define pb push_back
+typedef vector<int> vi;
+typedef vector<string> vs;
+typedef long long int ll;
+typedef unsigned long long int ull;
+typedef vector<ll> vll;
+typedef vector<ull> vull;
+typedef vector<bool> vb;
+const int MOD = 1e9+7;
+
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {    // O(n)
+      int l, m, h;
+      l = m = 0;
+      h = nums.size()-1;
+      while(m <= h) {
+        if(nums[m] == 0) {
+          swap(nums[l], nums[m]);
+          l++;
+          m++;
+        }
+        else if(nums[m] == 1) {
+          m++;
+        }
+        else {
+          swap(nums[m], nums[h]);
+          h--;
+        }
+      }
+    }
+};
+
+void solve(Solution sol) {
+    string line;
+    getline(cin, line); // Read the whole line
+
+    istringstream iss(line); // Create stream from line
+    vi nums;
+    int x;
+    while (iss >> x) {
+      nums.pb(x); // Extract numbers one by one
+    }
+    sol.sortColors(nums);
+    for(auto i: nums) {
+      cout << i << " ";
+    }    
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    int t = 1;
+    // cin >> t;
+    Solution obj = Solution();
+    while(t--) {
+        solve(obj);
+        cout << '\n';
+    }
+    return 0;
+}
