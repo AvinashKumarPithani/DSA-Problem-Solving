864223283b0a468dd0c8f192532982a9dedb9598
Pithani Avinash Kumar <avinashkumar.pithani@gmail.com>
2025-07-04 09:30:12 +0530

    max sum in subarray -- O(n)

---
+++ Arrays, Strings & Linked Lists/4. Kadane's Algorithm/1.1 maxSumSubarr.cpp
@@ -0,0 +1,62 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define endl '\n'
+#define FOR(i,a,b) for(int i=(a); i<(b); i++)
+#define FORk(i,a,b,k) for(int i=(a); i<b; i+=k)
+#define rFOR(i,a,b) for(int i=(a); i>=(b); i--)
+#define rFORk(i,a,b,k) for(int i=(a); i>=(b); i-=k)
+#define pb push_back
+typedef vector<int> vi;
+typedef vector<string> vs;
+typedef long long int ll;
+typedef unsigned long long int ull;
+typedef vector<ll> vll;
+typedef vector<ull> vull;
+typedef vector<bool> vb;
+const int MOD = 1e9+7;
+
+// https://leetcode.com/problems/maximum-subarray/
+
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {     // O(n) -- Kadane's Alg
+      int n = nums.size();    
+      ll sum = nums[0];
+      ll msum = sum;
+      FOR(i, 1, n) {
+        if(sum < 0) {
+          sum = 0;
+        }        
+        sum += nums[i];
+        msum = max(sum, msum);               
+      }
+      return msum;
+    }
+};
+
+void solve(Solution sol) {
+    string line;
+    getline(cin, line); // Read the whole line
+
+    istringstream iss(line); // Create stream from line
+    vi nums;
+    int x;
+    while (iss >> x) {
+      nums.pb(x); // Extract numbers one by one
+    }
+    cout << sol.maxSubArray(nums);
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    int t = 1;
+    // cin >> t;
+    Solution obj = Solution();
+    while(t--) {
+        solve(obj);
+        cout << '\n';
+    }
+    return 0;
+}
