7e3dbc9c31bfffcea90838676d4f2c1474cc919f
Pithani Avinash Kumar <avinashkumar.pithani@gmail.com>
2025-07-06 12:09:12 +0530

    Linked List Reversal -- O(n)

---
+++ Arrays, Strings & Linked Lists/11. Linked Lists/2. reverseLL.cpp
@@ -0,0 +1,85 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define endl '\n'
+#define FOR(i,a,b) for(int i=(a); i<(b); i++)
+#define FORk(i,a,b,k) for(int i=(a); i<b; i+=k)
+#define rFOR(i,a,b) for(int i=(a); i>=(b); i--)
+#define rFORk(i,a,b,k) for(int i=(a); i>=(b); i-=k)
+#define pb push_back
+typedef vector<int> vi;
+typedef vector<string> vs;
+typedef long long int ll;
+typedef unsigned long long int ull;
+typedef vector<ll> vll;
+typedef vector<ull> vull;
+typedef vector<bool> vb;
+const int MOD = 1e9+7;
+
+// https://leetcode.com/problems/reverse-linked-list/
+
+
+// Definition for singly-linked list.
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+      ListNode *p, *q, *r;
+      p = head;
+      q = r = nullptr;
+      while(p) {
+        r = q;
+        q = p;
+        p = p->next;
+        q->next = r;
+      }
+      return q;
+    }
+};
+
+void solve(Solution sol) {
+    string line;
+    getline(cin, line); // Read the whole line
+
+    istringstream iss(line); // Create stream from line
+    vi nums;
+    int x;
+    while (iss >> x) {
+      nums.pb(x); // Extract numbers one by one
+    }
+    if (nums.empty()) return;
+    ListNode *head = new ListNode(nums[0]);
+    ListNode *p = head;
+    int i = 1;
+    while(i < nums.size()){
+      p->next = new ListNode(nums[i]);
+      p = p->next;
+      i++;
+    }
+    head = sol.reverseList(head);
+    p = head;
+    while(p){
+      cout << p->val << " ";
+      p = p->next;
+    }
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    int t = 1;
+    // cin >> t;
+    Solution obj = Solution();
+    while(t--) {
+        solve(obj);
+        cout << '\n';
+    }
+    return 0;
+}
