6d6eea8cdb25c0b9c0fb08c885f33dfb79bafec8
Pithani Avinash Kumar <avinashkumar.pithani@gmail.com>
2025-07-16 12:13:42 +0530

    koko eat bananas -- O(n*m)

---
+++ Arrays, Strings & Linked Lists/12. Binary Search/3. kokoBananas.cpp
@@ -0,0 +1,70 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define endl '\n'
+#define FOR(i,a,b) for(int i=(a); i<(b); i++)
+#define FORk(i,a,b,k) for(int i=(a); i<b; i+=k)
+#define rFOR(i,a,b) for(int i=(a); i>=(b); i--)
+#define rFORk(i,a,b,k) for(int i=(a); i>=(b); i-=k)
+#define pb push_back
+typedef vector<int> vi;
+typedef vector<string> vs;
+typedef long long int ll;
+typedef unsigned long long int ull;
+typedef vector<ll> vll;
+typedef vector<ull> vull;
+typedef vector<bool> vb;
+const int MOD = 1e9+7;
+
+// https://www.geeksforgeeks.org/problems/koko-eating-bananas/1
+
+class Solution {
+  public:
+    int kokoEat(vector<int>& arr, int k) {      // O(n*m)
+      int n = arr.size();
+      int m = *max_element(arr.begin(), arr.end());
+      int s = m;
+      FOR(i, 1, m) {
+        int h = 0;
+        for(auto j : arr) {
+          h += ceil(j*1.0/i);
+        }
+        if(h <= k) {
+          s = i;
+          break;
+        }
+      }
+      return s;
+    }
+};
+
+void solve(Solution sol) {
+    string line;
+    getline(cin, line); // Read the whole line
+
+    istringstream iss(line); // Create stream from line
+    vi nums;
+    int x;
+    while (iss >> x) {
+      nums.pb(x); // Extract numbers one by one
+    }
+    int k;
+    cin >> k;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+    int res = sol.kokoEat(nums, k);
+    cout << res;
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    int t = 1;
+    cin >> t;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+    Solution obj = Solution();
+    while(t--) {
+        solve(obj);
+        cout << '\n';
+    }
+    return 0;
+}
