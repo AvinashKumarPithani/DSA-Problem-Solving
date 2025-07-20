9374c322db82f5cec1ffe3f42f3fa7cad265d946
Pithani Avinash Kumar <avinashkumar.pithani@gmail.com>
2025-07-20 10:06:43 +0530

    largestSumAfterKNegations -- O(k*log n + n*log n)

---
+++ Arrays, Strings & Linked Lists/14. Greedy Algorithms/3.1 maxSumKNegations.cpp
@@ -0,0 +1,68 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define endl '\n'
+#define FOR(i,a,b) for(int i=(a); i<(b); i++)
+#define FORk(i,a,b,k) for(int i=(a); i<b; i+=k)
+#define rFOR(i,a,b) for(int i=(a); i>=(b); i--)
+#define rFORk(i,a,b,k) for(int i=(a); i>=(b); i-=k)
+#define pb push_back
+typedef vector<int> vi;
+typedef vector<string> vs;
+typedef long long int ll;
+typedef unsigned long long int ull;
+typedef vector<ll> vll;
+typedef vector<ull> vull;
+typedef vector<bool> vb;
+const int MOD = 1e9+7;
+
+// https://leetcode.com/problems/maximize-sum-of-array-after-k-negations/
+
+class Solution {
+public:
+    int largestSumAfterKNegations(vector<int>& nums, int k) {   // O(k*log n + n*log n)
+      priority_queue<int, vi, greater<int>> pq(nums.begin(), nums.end());
+      while(k--) {
+        int x = pq.top();
+        pq.pop();
+        pq.push(-x);
+      }
+      int s = 0;
+      while(!pq.empty()) {
+        s += pq.top();
+        pq.pop();
+      }
+      return s;
+    }
+};
+
+void solve(Solution sol) {
+    string line;
+    getline(cin, line); // Read the whole line
+
+    istringstream iss(line); // Create stream from line
+    vi nums;
+    int x;
+    while (iss >> x) {
+      nums.pb(x); // Extract numbers one by one
+    }
+    int k;
+    cin >> k;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+    int res = sol.largestSumAfterKNegations(nums, k);
+    cout << res;
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    int t = 1;
+    cin >> t;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+    Solution obj = Solution();
+    while(t--) {
+        solve(obj);
+        cout << '\n';
+    }
+    return 0;
+}
