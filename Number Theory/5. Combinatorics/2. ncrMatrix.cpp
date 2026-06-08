3d68bb34c0b5ca0af8d5a968959f388b8011486e
Pithani Avinash Kumar <avinashkumar.pithani@gmail.com>
2026-06-08 12:20:19 +0530

    Computing nCr using Pascal's triangle, O(N^2 + t)

--- Number Theory/5. Combinatorics/2. ncrMatrix.cpp
+++ Number Theory/5. Combinatorics/2. ncrMatrix.cpp
@@ -13,23 +13,27 @@ typedef unsigned long long int ull;
 typedef vector<ll> vll;
 typedef vector<ull> vull;
 
+// Computing nCr using Pascal's triangle -- O(N^2) precomputation, O(1) query time for each test case
+
 const int MOD = 1e9+7;
 const int N = 10;
 
 ll ncrMat[N+1][N+1];
 
-void pre_compute() {
+void pre_compute() {  // O(N^2)
   ncrMat[0][0] = 1;
   FOR(i, 1, N+1) {
     ncrMat[i][0] = 1;
     ncrMat[i][i] = 1;
-    FOR(j, 1, N) {
+    FOR(j, 1, i) {  // iCj is valid only when j <= i, so we can run j from 1 to i
       ncrMat[i][j] = ncrMat[i-1][j] + ncrMat[i-1][j-1];
     }
   }
 }
 
-void printMat(){
+// Printing all nCr values to check the correctness of the ncrMat
+// nCr = n-1Cr + n-1Cr-1
+void printMat(){  // O(N^2)
   FOR(i, 0, N+1) {
     FOR(j, 0, N+1) {
       cout << ncrMat[i][j] << " ";
@@ -38,20 +42,20 @@ void printMat(){
   }
 }
 
-void solve() {
+void solve() {  // O(1)
   int n, r;
   cin >> n >> r;
   cout << ncrMat[n][r]; // O(1)
 }
 
-int main() {    // O(N + T)
+int main() {    // O(N^2 + t)
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
     int t = 1;
     cin >> t;
-    pre_compute(); // O(N)
-    // printMat();
+    pre_compute(); // O(N^2)
+    // printMat(); // O(N^2)
     while(t--) {
         solve();
         cout << '\n';
