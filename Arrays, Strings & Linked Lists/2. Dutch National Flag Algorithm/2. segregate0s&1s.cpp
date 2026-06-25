7922c1c7e39feb7ae6197393851201120973c9b7
Pithani Avinash Kumar <avinashkumar.pithani@gmail.com>
2026-06-25 13:31:54 +0530

    segregate the 0s to left  and 1s to right in the array, O(n)

---
+++ Arrays, Strings & Linked Lists/2. Dutch National Flag Algorithm/2. segregate0s&1s.cpp
@@ -0,0 +1,68 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define endl '\n'
+#define FOR(i,a,b) for(int i=(a); i<(b); i++)
+#define FORk(i,a,b,k) for(int i=(a); i<b; i+=k)
+#define rFOR(i,a,b) for(int i=(a); i>=(b); i--)
+#define rFORk(i,a,b,k) for(int i=(a); i>=(b); i-=k)
+#define pb push_back
+typedef vector<int> vi;
+typedef vector<string> vs;
+typedef long long int ll;
+typedef unsigned long long int ull;
+typedef vector<ll> vll;
+typedef vector<ull> vull;
+typedef vector<bool> vb;
+const int MOD = 1e9+7;
+
+// https://www.geeksforgeeks.org/problems/segregate-0s-and-1s5106/1
+// Given an array of 0s and 1s, segregate the 0s and 1s in the array such that all 0s are on the left side and all 1s are on the right side of the array.
+
+class Solution {
+  public:
+    void segregate0and1(vector<int> &arr) {  // O(n)
+        // code here
+        int l, m;
+        l = m = 0;
+        while(m < arr.size()) {
+            if(arr[m]==0) {
+                swap(arr[m], arr[l]);
+                m++;
+                l++;
+            }
+            else {
+                m++;
+            }
+        }
+    }
+};
+
+void solve(Solution sol) {  // O(n)
+    string line;
+    getline(cin, line); // Read the whole line
+
+    istringstream iss(line); // Create stream from line
+    vi nums;
+    int x;
+    while (iss >> x) {
+      nums.pb(x); // Extract numbers one by one
+    }
+    sol.segregate0and1(nums);
+    for(auto i: nums) {
+      cout << i << " ";
+    }    
+}
+
+int main() {  // O(t*n)
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    int t = 1;
+    // cin >> t;
+    Solution obj = Solution();
+    while(t--) {
+        solve(obj);
+        cout << '\n';
+    }
+    return 0;
+}
